d0f2b2503bdb31e6043eb29f9fb9a466bbd9b29d
Aneesa Gilani <aneesagilani@Aneesas-MacBook-Air.local>
2023-08-09 18:53:45 -0400

    add placeholders

---
+++ src/layouts/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <li className="User icon"><a href="/usericon"> UserIcon </a> </li>
+        <li className="Cart"><a href="/cart"> Cart</a></li>
+        <li className='Search bar'><a href='/search'>Search</a></li>
+
+      </div>
+    </header>
+
+
+  )
+}
+
+export default Nav;
